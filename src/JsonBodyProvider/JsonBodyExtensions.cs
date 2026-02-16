323d71d45a6601c1c1a38ad1071355e6db8d7d80
alphons <alphons@heijden.com>
2026-02-16 10:31:09 +0100

    new src dir

--- src/JsonBodyProvider/JsonBodyExtensions.cs
+++ src/JsonBodyProvider/JsonBodyExtensions.cs
@@ -2,15 +2,15 @@
 
 namespace JsonBodyProvider;
 
-    public static class JsonBodyExtensions
-    {
+public static class JsonBodyExtensions
+{
 	public static IServiceCollection AddJsonProviders(this IServiceCollection services, bool CorrectLists = false)
 	{
 		return services
 			.AddControllers(options =>
 			{
 				options.ValueProviderFactories.Add(new JsonBodyValueProviderFactory());
-				if(CorrectLists)
+				if (CorrectLists)
 				{
 					options.ModelBinderProviders.Insert(0, new NoFallbackListBinderProvider());
 					options.ModelBinderProviders.Insert(0, new EmptyStringPreservingBinderProvider());
